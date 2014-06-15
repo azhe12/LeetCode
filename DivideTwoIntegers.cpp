8091b8fc68c37c53db38f0974a6b34952f20fa3c
azhe_liu <azhe>
2014-06-15 22:26:22 +0800

    [Divide Two Integers]

--- DivideTwoIntegers.cpp
+++ DivideTwoIntegers.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
+    //int divide(int dividend, int divisor) {
+    int divide(int dividend2, int divisor2) {
+        long long dividend = dividend2;     //避免-21123123, 变成整数之后的溢出, 因此用long long来保存dividend, divisor
+        long long divisor = divisor2;
+
         if (divisor == 0 || dividend == 0)
             return 0;
         bool positive = true;
@@ -30,7 +34,6 @@ public:
             while (tmp <= remain) {
                 tmp *= 2;   //每次都对除数翻倍
                 res *= 2;   //对商也翻倍
-                cout<<tmp<<" "<<res<<endl;
             }
             remain -= tmp/2;    //计算剩余的数字
             total += res/2;     //已经超过，对商减半，然后累加
@@ -46,5 +49,7 @@ int main()
     Solution so;
     //cout<<so.divide(20,3)<<endl;
     //cout<<so.divide(-20,-3)<<endl;
-    cout<<so.divide(2147483647,1)<<endl;
+    //cout<<so.divide(2147483647,1)<<endl;
+    //cout<<so.divide(-999511578,-2147483648)<<endl;
+    cout<<so.divide(1,1)<<endl;
 }
