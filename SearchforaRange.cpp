582bf8d76f65138f088d314f8d3582759691e5a9
azhe_liu <azhe_liu@htc.com>
2014-06-17 14:58:04 +0800

    [Search for a Range]

---
+++ SearchforaRange.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int binarySearch(int A[], int l, int r, int target)
+    {
+        if (l > r)
+            return -1;
+        int mid = (l+r) / 2;
+        if (A[mid] == target) {
+            return mid;
+        } else if (A[mid] < target) {
+            return binarySearch(A, mid+1, r, target);
+        } else {
+            return binarySearch(A, l, mid-1, target);
+        }
+    }
+
+    vector<int> searchRange(int A[], int n, int target) {
+        int pos = -1;
+        pos = binarySearch(A, 0, n-1, target);
+        int l = pos;
+        int r = pos;
+        while (l-1 >= 0 && A[l-1] == target) l--;
+        while (r+1 <= n-1 && A[r+1] == target) r++;
+        vector<int> res;
+        res.push_back(l);
+        res.push_back(r);
+        return res;
+    }
+};
+
+int main()
+{
+    Solution so;
+    int A[] = {1};
+    vector<int> res;
+    res = so.searchRange(A, 1, 1);
+    for (int i = 0; i < res.size(); i++)
+        cout<<res[i]<<endl;
+}
