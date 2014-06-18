8a65373ae280d5f62b16b45607436c5ec8141088
azhe_liu <azhe_liu@htc.com>
2014-06-18 14:26:59 +0800

    [Combination Sum II]

--- CombinationSumII.cpp
+++ CombinationSumII.cpp
@@ -1,7 +1,9 @@
 /*  azhe
  *  2014/6/18
  *  https://oj.leetcode.com/problems/combination-sum/
- *  DFS
+ *  Note: DFS
+ *  1. 不能重复
+ *  2. 去重: 如果一个数字和前一个数字相等， 且前一个数字在当前的结果数组res中未被使用，那么此重复应该被跳过
  * */
 #include <iostream>
 #include <algorithm>
@@ -21,13 +23,20 @@ public:
         }
         for (int i = start; 
                 i < cand.size() && sum+cand[i] <= tar; i++) {
+            if (i > 0 && !used[i-1] && cand[i] == cand[i-1])    //如果上个数字在当前结果数组中未被使用 且 数字重复，则跳过此重复
+                continue;
+            used[i] = true;
             res.push_back(cand[i]);
-            dfs(cand, i, tar, sum+cand[i], res);    //dfs, 由于可以重复，因此新的start依然是i
+            dfs(cand, i+1, tar, sum+cand[i], res);  //不能重复，因此从start=i+1继续dfs
+            used[i] = false;
             res.pop_back();
         }
     }
-    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+    vector<vector<int> > combinationSum2(vector<int> &candidates, int target) {
         results.clear();
+        used.clear();
+        used.resize(candidates.size(), false);  //表示在当前一个结果数组中，一个数字是否使用过
+
         vector<int> res;
         sort(candidates.begin(), candidates.end()); //排序
         dfs(candidates, 0, target, 0, res);
@@ -35,4 +44,5 @@ public:
     }
 private:
     vector<vector<int> > results;
+    vector<bool> used;
 };
