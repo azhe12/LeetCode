894e59a302f5573d514ba1265f741cb8b00402fc
azhe_liu <azhe_liu@htc.com>
2014-06-25 14:06:22 +0800

    [Binary Tree Inorder Traversal]

---
+++ BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,31 @@
+//azhe 2014/6/25
+//https://oj.leetcode.com/problems/binary-tree-inorder-traversal/
+#include <vector>
+#include <iostream>
+using namespace std;
+/**
+ * Definition for binary tree
+ */
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+ 
+class Solution {
+public:
+    void inorder(TreeNode * root, vector<int> & res)
+    {
+        if (!root)
+            return;
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+};
